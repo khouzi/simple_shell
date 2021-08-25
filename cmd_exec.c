a612b8c158cc1710b1647f8488cf0a02ca6b18cc
khouzi <khouzem.boussaid@gmail.com>
2021-08-25 02:30:32 -0700

    cmd_exec.c

---
+++ cmd_exec.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+/**
+ * cmd_execute - execute builtins
+ * @args: command entered
+ * Return: the executing of one of the builtins
+ */
+
+int cmd_execute(char **args)
+{
+	int i;
+
+	int (*builtin_func[])(char **) = {
+		&cmd_cd,
+		&cmd_help,
+		&cmd_exit,
+		&cmd_env
+};
+
+char *builtin_str[] = {
+	"cd",
+	"help",
+	"exit",
+	"env"
+};
+
+
+	if (args[0] == NULL)
+{
+	return (1);
+}
+	for (i = 0; i <= 3; i++)
+{
+	if (_strcmp(args[0], builtin_str[i]) == 0)
+	{
+		return ((*builtin_func[i])(args));
+	}
+}
+	return (cmd_launch(args));
+}
