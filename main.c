85e8bd2b0c032376e87f6237343c5eb436f3979a
khouzi <khouzem.boussaid@gmail.com>
2021-08-25 02:32:52 -0700

    main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+/**
+ * main - Entry point of the program.
+ * @argc: number of arguments
+ * @argv: array of arguments.
+ * Return: triggers the loop fucntion
+ */
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv)
+{
+	cmd_loop();
+	return (EXIT_SUCCESS);
+}
+
