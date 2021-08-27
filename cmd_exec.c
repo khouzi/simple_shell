d8336c8c7afe76c8022dc775fa051c9e3d21a26b
khouzi <62845331+khouzi@users.noreply.github.com>
2021-08-26 19:01:23 -0700

    Update cmd_exec.c

--- cmd_exec.c
+++ cmd_exec.c
@@ -7,33 +7,36 @@
 
 int cmd_execute(char **args)
 {
-	int i;
+        int i;
 
-	int (*builtin_func[])(char **) = {
-		&cmd_cd,
-		&cmd_help,
-		&cmd_exit,
-		&cmd_env
+        int (*builtin_func[])(char **) = {
+                &cmd_cd,
+                &cmd_help,
+                &cmd_exit,
+                &cmd_env,
+                &cmd_history
 };
 
 char *builtin_str[] = {
-	"cd",
-	"help",
-	"exit",
-	"env"
+        "cd",
+        "help",
+        "exit",
+        "env",
+        "history"
 };
 
 
-	if (args[0] == NULL)
+        if (args[0] == NULL)
 {
-	return (1);
+        return (1);
 }
-for (i = 0; i <= 3; i++)
+        for (i = 0; i <= 4; i++)
 {
-	if (strcmp(args[0], builtin_str[i]) == 0)
-	{
-		return ((*builtin_func[i])(args));
-	}
+        if (strcmp(args[0], builtin_str[i]) == 0)
+        {
+                return ((*builtin_func[i])(args));
+        }
+
 }
-	return (cmd_launch(args));
+        return (cmd_launch(args));
 }
