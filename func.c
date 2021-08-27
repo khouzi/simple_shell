03490b640beaca3d36369ad7206e4ef8a5118008
khouzi <62845331+khouzi@users.noreply.github.com>
2021-08-26 19:04:18 -0700

    Update func.c

--- func.c
+++ func.c
@@ -7,38 +7,37 @@
  */
 size_t _strlen(const char *s)
 {
-	int i;
+        int i;
 
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return ((i + 1));
+        i = 0;
+        while (s[i] != '\0')
+        {
+                i++;
+        }
+        return ((i + 1));
 }
 
 /**
- * _strcmp - compare two strings
- * @s1: string one
- * @s2: string two
- * Return: returns an integer less  than,  equal  to,  or
- * greater  than zero if s1 is found, respectively, to be
- * less than, to match, or be greater than s2.
+ * _strcmp - compares two strings
+ * @s1: first string, of two, to be compared in length
+ * @s2: second string, of two, to be compared
+ * Return: 0 on success, anything else is a failure
  */
-
 int _strcmp(char *s1, char *s2)
 {
-	char *p1 = s1;
-	char *p2 = s2;
+        int i;
 
-	while (*p1 != '\0' && *p2 != '\0' && *p1 == *p2)
-	{
-		p1++;
-		p2++;
-	}
-	return (*p1 - *p2);
+        i = 0;
+        while (s1[i] == s2[i])
+        {
+                if (s1[i] == '\0')
+                {
+                        return (0);
+                }
+                i++;
+        }
+        return (s1[i] - s2[i]);
 }
-
 /**
  * _strdup - copies a string to another
  * @duplicate: the array
@@ -46,22 +45,20 @@ int _strcmp(char *s1, char *s2)
  */
 char *_strdup(char *duplicate)
 {
-	char *copy;
-	int len, i;
+        char *copy;
+        int len, i;
 
-	if (duplicate == 0)
-		return (NULL);
+        if (duplicate == 0)
+                return (NULL);
 
-	for (len = 0; duplicate[len]; len++)
-		;
-	copy = malloc((len + 1) * sizeof(char));
+        for (len = 0; duplicate[len]; len++)
+                ;
+        copy = malloc((len + 1) * sizeof(char));
+        for (i = 0; i <= len; i++)
+                copy[i] = duplicate[i];
 
-	for (i = 0; i <= len; i++)
-		copy[i] = duplicate[i];
-
-	return (copy);
+        return (copy);
 }
-
 /**
  * read_textfile - function that reads a text file and prints
  * it to the POSIX standard output.
@@ -72,37 +69,38 @@ char *_strdup(char *duplicate)
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	size_t read_buf, printed;
-	char *buf = malloc(sizeof(char) * letters);
+        int fd;
+        size_t read_buf, printed;
+        char *buf = malloc(sizeof(char) * letters);
 
-	if (buf == NULL)
-	{
-		return (-1);
-	}
-	if (filename == NULL)
-		return (-1);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		free(buf);
-		return (-1);
-	}
-	read_buf = read(fd, buf, letters);
-	if (read_buf == -1UL)
-	{
-		free(buf);
-		return (-1);
-	}
-	printed = write(STDOUT_FILENO, buf, read_buf);
-	if (printed == -1UL)
-	{
-		return (-1);
-	}
-	write(1, "\n", 1);
-	free(buf);
-	close(fd);
-	return (printed);
+        if (buf == NULL)
+        {
+                return (-1);
+        }
+        if (filename == NULL)
+                return (-1);
+        fd = open(filename, O_RDONLY);
+        if (fd == -1)
+        {
+                free(buf);
+                return (-1);
+        }
+        read_buf = read(fd, buf, letters);
+        if (read_buf == -1UL)
+        {
+                free(buf);
+                return (-1);
+        }
+        printed = write(STDOUT_FILENO, buf, read_buf);
+        if (printed == -1UL)
+        {
+                free(buf);
+                return (-1);
+                              }
+        write(1, "\n", 1);
+        free(buf);
+        close(fd);
+        return (printed);
 }
 /**
  * free_array - frees an array
@@ -110,7 +108,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
  */
 void free_array(char **str)
 {
-	int i;
+        int i;
 
 for (i = 0; str[i] != NULL; i++)
 {
@@ -118,3 +116,4 @@ free(str[i]);
 }
 free(str);
 }
+
